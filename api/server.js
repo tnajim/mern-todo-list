9d3f48a876fda940614846ccc5521bcdf7dc61a6
Taher Najim <taher.najim@yahoo.com>
2023-09-01 17:37:29 +0800

    created get post and delete todo routes

--- api/server.js
+++ api/server.js
@@ -6,14 +6,33 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const dbUrl = "mongodb://127.0.0.1:27017/mern-todo"; // for deployment use .process.env.DB_URL
+const dbUrl = "mongodb://127.0.0.1:27017/mern-todo"; // for deployment, use .process.env.DB_URL
 // const dbUrl ='mongodb://127.0.0.1:27017/mern-todo'; // (local mongodb)
 
 mongoose.connect(dbUrl)
     .then(() => console.log("MongoDB Connection Established."))
     .catch(error => console.log("MongoDB Error Occured.", error));
 
+const Todo = require('./models/Todo');
 
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+})
+
+app.post('/todos/new', async (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    await todo.save();
+    res.json(todo);
+})
+
+app.delete('/todos/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await Todo.findByIdAndDelete(id);
+    res.json(result);
+})
 
 // listen to server
 app.listen(3000, () => {
